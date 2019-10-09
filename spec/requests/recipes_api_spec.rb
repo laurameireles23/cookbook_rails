6814b08f8803c9ded73e6dd3416198afa9f00d3b
LucasKendi <hiranolucas@gmail.com>
2019-10-09 09:48:49 -0300

    adjust tests order

--- spec/requests/recipes_api_spec.rb
+++ spec/requests/recipes_api_spec.rb
@@ -24,29 +24,6 @@ describe 'Recipes Api' do
             expect(json_recipes[1][:title]).to eq 'Tapioca de maniçoba'
         end
 
-        it 'view one recipe' do
-            user = User.create!(email: 'visit@visit.com', password: '123456', role: :user)
-            recipe_type = RecipeType.create(name: 'Entrada')
-            recipe = Recipe.create(status: :pending, user: user, title: 'Tapioca apimentada', difficulty: 'Dificil',
-            recipe_type: recipe_type, cuisine: 'Baiana',
-            cook_time: 50, ingredients: 'Farinha de tapioca, pimenta',
-            cook_method: 'Hidrate a goma, prepare na frigideira a tapioca, encha de pimenta e sirva')
-
-            other_recipe = Recipe.create(status: :pending, user: user, title: 'Tapioca de maniçoba', difficulty: 'Dificil',
-            recipe_type: recipe_type, cuisine: 'Baiana',
-            cook_time: 50, ingredients: 'Farinha de tapioca, maniçoba',
-            cook_method: 'Hidrate a goma, prepare na frigideira a tapioca, encha de soco e sirva')
-
-            get api_v1_recipe_path(recipe)
-
-            json_recipes = JSON.parse(response.body, symbolize_names: true)
-
-            expect(response).to have_http_status(:ok)
-            expect(response.content_type).to eq 'application/json'
-            expect(json_recipes[:title]).to eq 'Tapioca apimentada'
-            expect(json_recipes[:title]).not_to eq 'Tapioca de maniçoba'
-        end
-
         it 'and recipes are approved' do
             user = User.create!(email: 'visit@visit.com', password: '123456', role: :user)
             recipe_type = RecipeType.create(name: 'Entrada')
@@ -77,6 +54,31 @@ describe 'Recipes Api' do
             expect(json_recipes[1][:title]).to eq 'Geleia de mocotó'
             expect(json_recipes[1][:status]).to eq 'approved'
             expect(response.body).not_to include('Sorvete de coentro')
+        end
+    end
+
+    context 'index' do
+        it 'view one recipe' do
+            user = User.create!(email: 'visit@visit.com', password: '123456', role: :user)
+            recipe_type = RecipeType.create(name: 'Entrada')
+            recipe = Recipe.create(status: :pending, user: user, title: 'Tapioca apimentada', difficulty: 'Dificil',
+            recipe_type: recipe_type, cuisine: 'Baiana',
+            cook_time: 50, ingredients: 'Farinha de tapioca, pimenta',
+            cook_method: 'Hidrate a goma, prepare na frigideira a tapioca, encha de pimenta e sirva')
+
+            other_recipe = Recipe.create(status: :pending, user: user, title: 'Tapioca de maniçoba', difficulty: 'Dificil',
+            recipe_type: recipe_type, cuisine: 'Baiana',
+            cook_time: 50, ingredients: 'Farinha de tapioca, maniçoba',
+            cook_method: 'Hidrate a goma, prepare na frigideira a tapioca, encha de soco e sirva')
+
+            get api_v1_recipe_path(recipe)
+
+            json_recipes = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:ok)
+            expect(response.content_type).to eq 'application/json'
+            expect(json_recipes[:title]).to eq 'Tapioca apimentada'
+            expect(json_recipes[:title]).not_to eq 'Tapioca de maniçoba'
         end
     end
 end
